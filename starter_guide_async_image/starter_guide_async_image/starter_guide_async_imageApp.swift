44d1204401b028ac197d4b9f2bcd6930f37ab029
rookedsysc <rookedsysc36@gmail.com>
2023-01-30 10:14:26 +0900

    feat : create project

---
+++ starter_guide_async_image/starter_guide_async_image/starter_guide_async_imageApp.swift
@@ -0,0 +1,17 @@
+//
+//  starter_guide_async_imageApp.swift
+//  starter_guide_async_image
+//
+//  Created by Rookedsysc on 2023/01/30.
+//
+
+import SwiftUI
+
+@main
+struct starter_guide_async_imageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

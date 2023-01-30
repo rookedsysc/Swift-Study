291e2eda793972926140e053fa4e31e365002015
rookedsysc <rookedsysc36@gmail.com>
2023-01-30 10:19:57 +0900

    feat : image scale 변경

--- starter_guide_async_image/starter_guide_async_image/ContentView.swift
+++ starter_guide_async_image/starter_guide_async_image/ContentView.swift
@@ -11,7 +11,9 @@ struct ContentView: View {
     private let imageUrl: String = "https://credo.academy/credo-academy@3x.png"
     
     var body: some View {
-        AsyncImage(url: URL(string: imageUrl))
+        // 값이 커질수록 이미지가 작아짐
+        // 값이 1보다 작으면 이미지가 커짐
+        AsyncImage(url: URL(string: imageUrl), scale: 3.0 )
     }
 }
 
